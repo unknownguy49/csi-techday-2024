abf3bb8272de4743910072d2e90fb1c3d90c835a
Unknown Guy <91245369+unknownguy49@users.noreply.github.com>
2024-11-06 16:55:32 +0530

    dark-mode 200

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+    const body = document.body;
 
+    darkModeToggle.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+        darkModeToggle.querySelector('i').classList.toggle('fa-sun');
+        darkModeToggle.querySelector('i').classList.toggle('fa-moon');
+        darkModeToggle.querySelector('i').classList.toggle('light-mode'); 
+    });
+});
